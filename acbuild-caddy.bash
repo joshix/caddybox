7eb1c31070d8bc3ff15dca93ce68e5a59fb7d3ef
Joshua Wood <j@joshix.com>
2016-02-28 14:33:39 -0800

    Update acbuild script for v0.8.2

--- acbuild-caddy.bash
+++ acbuild-caddy.bash
@@ -37,4 +37,4 @@ acbuild --debug mount add resolv /etc/resolv.conf
 acbuild --debug set-exec -- /bin/caddy -root /var/www/html
 
 # Save the ACI
-acbuild --debug write --overwrite caddy-v0.8.1-linux-amd64.aci
+acbuild --debug write --overwrite caddy-v0.8.2-linux-amd64.aci
