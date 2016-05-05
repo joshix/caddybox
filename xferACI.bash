8338426b32cd7a5e0870b635a220d21a48766d5d
Josh Wood <j@coreos.com>
2016-05-05 16:17:13 -0700

    xferACI.bash: Make it a script(ish(er))

--- xferACI.bash
+++ xferACI.bash
@@ -1,9 +1,8 @@
-# Obviously not an actual script yet
+#!/bin/bash
 
-$ cd $HOME/go/src/github.com/joshix/acbuild
-towncar:acbuild j$ scp -i .vagrant/machines/default/virtualbox/private_key -P 2222 \
-vagrant@127.0.0.1:caddybox/caddy-v0.8.2-linux-amd64.aci ~/Downloads/
-caddy-v0.8.2-linux-amd64.aci                  100% 3866KB   3.8MB/s   00:00
-towncar:acbuild j$ scp ~/Downloads/caddy-v0.8.2-linux-amd64.aci core@jxnu.joshix.com:
-caddy-v0.8.2-linux-amd64.aci                  100% 3866KB   1.3MB/s   00:03
-towncar:acbuild j$
+cd $GOPATH/src/github.com/joshix/acbuild
+
+scp -i .vagrant/machines/default/virtualbox/private_key -P 2222 \
+vagrant@127.0.0.1:caddybox/caddy-v0.8.3-linux-amd64.aci ~/Downloads/
+
+scp ~/Downloads/caddy-v0.8.3-linux-amd64.aci core@jxnu.joshix.com:
