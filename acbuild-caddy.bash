2d91cf9d6b61cec53b7f82af7406bacc20de5272
Joshua Wood <j@joshix.com>
2016-06-20 01:51:36 -0700

    Caddy beta: v0.9beta.1

--- acbuild-caddy.bash
+++ acbuild-caddy.bash
@@ -34,4 +34,4 @@ acbuild --debug mount add dotcaddy /root/.caddy
 acbuild --debug set-exec -- /bin/caddy -root /var/www/html
 
 # Save the ACI
-acbuild --debug write --overwrite caddy-v0.8.3-linux-amd64.aci
+acbuild --debug write --overwrite caddy-v0.9beta.1-linux-amd64.aci
