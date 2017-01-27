c321c179f13451b6c269202d2d77d7570c118600
Joshua Wood <j@joshix.com>
2017-01-27 05:25:31 -0800

    Delete acbuild scripts and notes

--- xferACI.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ACI=caddy-v0.9.5-linux-amd64.aci
-SSH=core@jxnu.joshix.com
-
-scp -i $GOPATH/src/github.com/joshix/acbuild/.vagrant/machines/default/virtualbox/private_key -P 2222 \
-vagrant@127.0.0.1:caddybox/$ACI .
-
-scp $ACI $SSH:
