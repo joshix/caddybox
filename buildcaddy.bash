90a0addc52964296b0d22172219d40c436aa8b50
Joshua Wood <j@joshix.com>
2018-04-26 03:21:08 -0500

    Caddy v0.10.14: Update binary and references

--- buildcaddy.bash
+++ buildcaddy.bash
@@ -2,7 +2,7 @@
 
 # Builds caddy binary for caddybox rootfs using caddy's build.go.
 #
-# Usage: ./buildcaddy.bash $tag (Ex: v0.10.12)
+# Usage: ./buildcaddy.bash $tag (Ex: v0.10.14)
 # Assumes cwd is go/src/github.com/joshix/caddybox.
 
 tag=$1
