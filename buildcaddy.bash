f1963a6a23fd3c0dca32c26ea69de3081d5d9d3b
Joshua Wood <j@joshix.com>
2019-02-03 16:56:04 -0800

    Caddy v0.11.2: Update binary and references.
    Minimally address https://github.com/joshix/caddybox/pull/45 in
    README.md.

--- buildcaddy.bash
+++ buildcaddy.bash
@@ -2,7 +2,7 @@
 
 # Builds caddy binary for caddybox rootfs using caddy's build.go.
 #
-# Usage: ./buildcaddy.bash $tag (Ex: v0.11.1)
+# Usage: ./buildcaddy.bash $tag (Ex: v0.11.2)
 # Assumes cwd is go/src/github.com/joshix/caddybox.
 
 tag=$1
